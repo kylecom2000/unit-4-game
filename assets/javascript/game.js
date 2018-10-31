c01a8381d718b8ed08733c1e5799f573a3d51d82
FightingSprrow <kbauertx@gmail.com>
2018-10-31 18:57:46 -0500

    added CSS styling and some variable in JS.

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+// Generate random number
+
+// Display rondom number
+
+// check if randomNumber === userNumber
+
+// if true, add to wins, start over random number generator
+
+// if false keep game going
+
+// if user number is > randomNumber then add to losses.
+
+
+
+
+//variables
+var wins = 0;
+var losses = 0;
+
+// generate random numbers for gems
+// assign to gems
+
