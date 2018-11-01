9a5416ab899d71b8059e81d824c4a500893d20dc
FightingSprrow <kbauertx@gmail.com>
2018-10-31 22:38:37 -0500

    created buttons and a for loop for crystal numbers.

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # unit-4-game
 ## jQuery Assignment
 ### Option One: CrystalsCollector Game (Recommended)
-*
+####Access game [here](https://kylecom2000.github.io/unit-4-game/).
+
+*Game uses javascript and jQuery to run the functions and modify HTML for the game.
+
+*CSS styling using Bootstrap
+
+
+
+
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,70 @@
-// Generate random number
+// Crystal Collector Game
+// How to Play
+// You will be given a random number at the start of the game.
+// There are four crystals below. By clicking on a crystal, 
+// you will add a specific amount of points to your total score.
+// You win the game by matching your total score to the random number.
+// You lose the game if your total score goes above the random number.
+// The value of each crystal is hidden from you and generated at random at the beginnig of each game.
 
-// Display rondom number
+// Each game:
+// Crystals need random numbers from 1-12 
+// Generate/display random number from 19-120
 
 // check if randomNumber === userNumber
-
 // if true, add to wins, start over random number generator
-
 // if false keep game going
-
 // if user number is > randomNumber then add to losses.
+// The game restarts if player wins or loses.
 
 
+
+// Crystal array instead of crystals on page?
 
 
 //variables
 var wins = 0;
 var losses = 0;
+var randomNumber;
+var playerTotal = "";
 
-// generate random numbers for gems
-// assign to gems
+
+
+
+
+// a function obejct to run the game.
+$(document).ready(function() {
+
+    // Generate random number 
+    randomNumber = Math.floor(Math.random() * (120-20)) + 20;
+    console.log(randomNumber);
+    
+    //give each button a value from a random number
+    for (var i = 0; i < 4; i++){
+        var crystalNumber = Math.floor(Math.random() * 12) + 1;
+        console.log(crystalNumber);
+        //assign to each crystal value ID, crystal0, 1, 2, 3.
+        $("#crystal" + i).attr('value', crystalNumber);
+    }
+
+    //HTML IDs random, wins, losses, playerTotal
+    //  post random number to html
+    $("#random").text(randomNumber);
+
+    
+  
+
+    $(".button").click(function(){
+        //add value of button to playerTotal
+        playerTotal += $(this).val();
+        $("#playerTotal").text(playerTotal);
+    });
+
+
+
+
+
+
+});
+
 
